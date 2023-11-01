bcb5f2ae581cab085d26dd0ec7b19fafe87bcaea
Satishkumaryadav2001 <sk3407392@gmail.com>
2023-11-01 09:17:49 +0530

    Main.jsx Toaster Add

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 
 //Component import
+import { Toaster } from 'react-hot-toast';
 import App from './App.jsx'
 //CSS imports
 import './index.css'
@@ -11,6 +12,7 @@ import {BrowserRouter} from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
 <BrowserRouter>
 <App />
+<Toaster/>
 </BrowserRouter>
    
 
