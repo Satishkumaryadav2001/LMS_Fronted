9724067523ddfd2ed686d4db62b6fac6360a3754
Satishkumaryadav2001 <sk3407392@gmail.com>
2023-11-12 07:29:25 +0530

    Cleaned up our about us

---
+++ src/Constants/CelebrityData.js
@@ -0,0 +1,76 @@
+import Carsousel_l from '../assets/Image/carousel-1.avif';
+import Carsousel_2 from '../assets/Image/carousel-2.avif';
+import Carsousel_3 from '../assets/Image/carousel-3.avif';
+import Carsousel_4 from '../assets/Image/carousel-4.avif';
+import Carsousel_5 from '../assets/Image/carousel-5.avif';
+import Carsousel_6 from '../assets/Image/carousel-6.avif';
+import Carsousel_7 from '../assets/Image/carousel-7.avif';
+import Carsousel_8 from '../assets/Image/carousel-8.avif';
+import Carsousel_9 from '../assets/Image/carousel-9.avif';
+import Carsousel_10 from '../assets/Image/carousel-10.avif';
+
+export const celebrities=[
+    {
+    title:` Sardar Vallabhai Patel`,
+    description:`"To me there is only one answer to this questions. That answer is not that you should shut yourselves in colleges and learn history and mathematics while the country is on fire and everybody is fighting freedom's battle. Your place is by the side of your countrymen, who are fighting the freedom's battle."`,
+    image:Carsousel_l,
+    slideNumber:1
+  },
+  {
+    title:` Mohandas Karamchand Gandhi`,
+    description:`"Literacy in itself is no education. Literacy is not the end of education or even the beginning. By education I mean an all-round drawing out of the best in the child and man-body, mind and spirit."`,
+    image:Carsousel_2,
+    slideNumber:2
+  },
+  {
+    title:` Dr. A.P.J. Abdul Kalam`,
+    description:`"Educationists should build the capacities of the spirit of inquiry, creativity, entrepreneurial and moral leadership among students and become their role model."`,
+    image:Carsousel_3,
+    slideNumber:3
+  },
+  {
+    title:` Dr B R Ambedkar`,
+    description:`“The progress of any society depends on the progress of education in that society” “Knowledge is the basis of human life. To enhance the intellectual capacity of the students; Also, every effort should be made to increase their intelligence”. “You got educated doesn't mean everything is done."`,
+    image:Carsousel_4,
+    slideNumber:4
+  },
+  {
+    title:` Khan Sir`,
+    description:`“Your belief in yourself should always be unshakable, indeed when faced with challenges.”`,
+    image:Carsousel_5,
+    slideNumber:5
+  },
+  {
+    title:`  Sundar Pichai`,
+    description:`“You might fail a few times, but that’s Ok. You end up doing something worthwhile which you learn a great deal from.”`,
+    image:Carsousel_6,
+    slideNumber:6
+  },
+  {
+    title:`Alakh Pandey sir`,
+    description:`"Kisi bhi competetion k liye ye baat important nhi hoti k aap ne kitni mehnat put in ki...ye baat imp hoti hai k agl bagal wale kitni mehnat kar rahe hain...Bhai ye competetion hai ...ye competetion hai tu 6 kar raha hai udhar jhank k dekh wo 12 wo 18 udhar jhank kr dekh 12 with planning udhar jhank kr dekh 14 ghante with planning r tu apne aap ko dekh unmanaged 6 ghante alternate day pr ...kaise hoga selection."`,
+    image:Carsousel_7,
+    slideNumber:7
+  },
+  {
+    title:` Ojha Sir`,
+    description:`"Make yourself strong, mind and body, this will help you win in the battle of life.."`,
+    image:Carsousel_8,
+    slideNumber:8
+  },
+  {
+    title:` Vikash divyakirti`,
+    description:`" What is called the world is a magic toy, what is found is clay, what is not found is gold."`,
+    image:Carsousel_9,
+    slideNumber:9
+
+  }
+ ,
+
+  {
+    title:` MS Dhoni `,
+    description:`“I have three dogs at home. Even after losing a series or winning a series, they treat me the same way.” `,
+    image:Carsousel_10,
+    slideNumber:10
+  }
+]
